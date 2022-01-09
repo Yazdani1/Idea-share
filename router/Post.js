26dba07aa5fc02ae1d4f84a98b83d435a32eaf1c
Yazdani <yaz4dani@gmail.com>
2022-01-09 21:01:39 +0200

    Controller

--- router/Post.js
+++ router/Post.js
@@ -1,52 +1,12 @@
 const router = require("express").Router();
 const Post = require("../model/Post");
 
-router.post("/post", (req, res) => {
-  const { title, des } = req.body;
+const postController = require("../controller/post");
 
-  if (!title) {
-    return res.status(422).json({ error: "Please add post title" });
-  }
+router.post("/post", postController.createPost);
 
-  if (!des) {
-    return res.status(422).json({ error: "Please add post des" });
-  }
+router.get("/getposts", postController.getPosts);
 
-  const postdata = Post({
-    title,
-    des,
-  });
-
-  Post.create(postdata)
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.get("/getposts", (req, res) => {
-  Post.find({})
-    .sort({ date: "DESC" })
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.delete("/delete/:id", (req, res) => {
-  var deletequery = { _id: req.params.id };
-
-  Post.findByIdAndDelete(deletequery)
-    .then((result) => {
-      res.json({ message: "Post deleted successfully" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.delete("/delete/:id",postController.deletePost); 
 
 module.exports = router;
