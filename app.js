3e5a3b1cd34662ab782f5d612af22b37947b6483
Yazdani <yaz4dani@gmail.com>
2022-01-09 21:44:23 +0200

    version1.1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -18,8 +18,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
     // "dev": "concurrently \"npm run server\" \"npm start --prefix client\""
 
+    //"proxy": "http://localhost:5000"
 app.use("/api",require("./router/Post"));
 
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 app.listen(port, (req, res) => {
--- model/db.js
+++ model/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-mongoose.connect('mongodb://localhost:27017/ideashare',{
+mongoose.connect(process.env.MONGOATLASURL,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 },(err)=>{
