bf1274d40d1f5a62a504d87601a7a258dd1ad14e
Yazdani <yaz4dani@gmail.com>
2022-01-10 12:34:29 +0200

    Deploy to Vercel

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
+
 
 // "proxy": "http://localhost:8080"
 // "dev": "concurrently \"npm run server\" \"npm start --prefix client\""
@@ -20,14 +22,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api",require("./router/Post"));
 
-
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+//deploy for heroku
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+//to deploy vercel
+
+if(process.env.NODE_ENV=='production'){
+  const path = require('path')
+
+  app.get('/',(req,res)=>{
+      app.use(express.static(path.resolve(__dirname,'client','build')))
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
 }
 
 
